1b10ae42a3835378011854deec83e4875e0719df
thair alokaily <thairalokaily@gmail.com>
2019-02-02 00:03:49 -0800

    generat random colors and fixing some styles

--- colorgame.js
+++ colorgame.js
@@ -1,17 +1,11 @@
 
 
-let colors = [
-    'rgb(255, 0, 0)',
-    'rgb(255, 255, 0)',
-    'rgb(255, 0, 255)',
-    'rgb(255, 255, 255)',
-    'rgb(255, 0, 0)',
-    'rgb(255, 0, 0)'
-];
+let colors = generatColors(6);
 
 const squares = document.querySelectorAll(".square");
 const colorDisplay = document.getElementById("colorDisplay");
 const messageDisplay = document.getElementById("message");
+const h1 = document.querySelector("h1");
 let pickedColor = pickcolor();
 
 colorDisplay.textContent = pickedColor;
@@ -23,9 +17,11 @@ for (let i = 0; i < squares.length; i++){
     //add click listeners to squares
     squares[i].addEventListener("click", function() {
         let clickedColor = this.style.backgroundColor;
+        
         if(clickedColor === pickedColor) {
         messageDisplay.textContent="correct";
         changColors(pickedColor);
+        h1.style.backgroundColor = pickedColor;
        
         }
         else {
@@ -37,6 +33,33 @@ for (let i = 0; i < squares.length; i++){
 
 }
 
+
+function generatColors(mod){
+    //make an array
+    let arr = [];
+    // add rand colors to arr
+    for (let i = 0; i < mod; i++){
+        arr.push(randColors());
+    }
+    //return the same array
+    return arr;
+
+}
+
+function randColors(){
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return 'rgb(' + red + ', ' + green + ', ' + blue + ')';
+
+}
+
+function pickcolor(){
+    let ran = Math.floor(Math.random() * colors.length );
+    return colors[ran];
+}
+
+
 function changColors(color){
     //loop all the squars 
     for (let i = 0; i < squares.length; i++){
@@ -44,10 +67,4 @@ function changColors(color){
         squares[i].style.backgroundColor = color;
     }
     
-}
-
-
-function pickcolor(){
-    let ran = Math.floor(Math.random() * colors.length );
-    return colors[ran];
 }
